93bdaac49de2ec445e73a2ff3d46dfe68417c79d
전용준 <65535yj@gmail.com>
2021-11-19 00:19:18 +0900

    CRUD

--- MMO_EFCore/MMO_EFCore/DbCommands.cs
+++ MMO_EFCore/MMO_EFCore/DbCommands.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Storage;
 
@@ -19,6 +21,7 @@ namespace MMO_EFCore
                 db.Database.EnsureDeleted();
                 db.Database.EnsureCreated();
 
+                CreateTestData(db);
                 Console.WriteLine("DB Initialized");
             }
         }
@@ -54,6 +57,60 @@ namespace MMO_EFCore
 
             db.Items.AddRange(items);
             db.SaveChanges();
+        }
+
+        public static void ReadAll()
+        {
+            using (var db = new AppDbContext())
+            {
+                // AsNoTracking : ReadOnly << Tracking SnapShot이라고 데이터 변경 탐지하는 기능 때문
+                // Include : Eager Loading (즉시 로딩) << 나중에 알아볼 것
+                foreach (Item item in db.Items.AsNoTracking().Include(i => i.Owner))
+                {
+                    Console.WriteLine($"TemplateId({item.TemplateId}) Owner({item.Owner.Name}) Created({item.CreateDate})");
+                }
+            }
+        }
+
+        // 특정 플레이어가 소지한 아이템들의 CreateDate를 수정
+        public static void UpdateDate()
+        {
+            Console.WriteLine("Input Player Name");
+            Console.WriteLine("> ");
+            string name = Console.ReadLine();
+
+            using (var db = new AppDbContext())
+            {
+                var items = db.Items.Include(i => i.Owner)
+                    .Where(i => i.Owner.Name == name);
+
+                foreach (Item item in items)
+                {
+                    item.CreateDate = DateTime.Now;
+                }
+
+                db.SaveChanges();
+            }
+
+            ReadAll();
+        }
+
+        public static void DeleteItem()
+        {
+            Console.WriteLine("Input Player Name");
+            Console.WriteLine("> ");
+            string name = Console.ReadLine();
+
+            using (var db = new AppDbContext())
+            {
+                var items = db.Items.Include(i => i.Owner)
+                    .Where(i => i.Owner.Name == name);
+
+                db.Items.RemoveRange(items);
+                db.SaveChanges();
+            }
+
+            ReadAll();
         }
     }
 }
--- MMO_EFCore/MMO_EFCore/Program.cs
+++ MMO_EFCore/MMO_EFCore/Program.cs
@@ -11,6 +11,32 @@ namespace MMO_EFCore
             DbCommands.InitializeDB(forceReset: true);
 
             // CRUD (Create-Read-Update-Delete)
+            Console.WriteLine("명령어를 입력하세요");
+            Console.WriteLine("[0] Force Reset");
+            Console.WriteLine("[1] ReadAll");
+            Console.WriteLine("[2] UpdateDate");
+            Console.WriteLine("[3] DeleteItem");
+
+            while(true)
+            {
+                Console.Write("> ");
+                string command = Console.ReadLine();
+                switch (command)
+                {
+                    case "0":
+                        DbCommands.InitializeDB(forceReset: true);
+                        break;
+                    case "1":
+                        DbCommands.ReadAll();
+                        break;
+                    case "2":
+                        DbCommands.UpdateDate();
+                        break;
+                    case "3":
+                        DbCommands.DeleteItem();
+                        break;
+                }
+            }
         }
     }
 }
